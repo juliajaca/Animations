3470d4deef7d05b286d58a61babbae19d31bfcf0
jjaca <jjaca@cifpfbmoll.eu>
2021-12-01 17:12:52 +0100

    animacion tween básica tutorial UPV

---
+++ app/src/main/java/com/example/animations/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.animations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        texto.startAnimation(animacion);
+    }
+}
